4764cdbe58ec64b034c576ab7e14a3b3f8c2713e
ahmed gamal <49871250+ahmedgamalelazab@users.noreply.github.com>
2023-05-05 17:15:14 +0300

    Stash 2023 05 05 17 14 04 (#1)
    * solved max sum and avrg using sliding window pattern

--- index.ts
+++ index.ts
@@ -10,19 +10,31 @@ interface ISlidingWindow {
   locked: boolean;
   locakWindowAtIndex: number;
   sum: number;
+  store?: number;
   result: number[];
   updateWindow: (
     window: ISlidingWindow,
     capturedItem: number,
-    capturedItemIndex: number
+    capturedItemIndex: number,
+    targetArr: number[]
   ) => void; // update index, sum, result, capture state
 }
 
+//problem 1
 // given array : [1, 3, 2, 6, -1, 4, 1, 8, 2]
 // required find the avr of each subarray in the list if the k is = 5
 
+//problem 2
+//given array [2, 1, 5, 1, 3, 2]
+// required to calculate the maximum subarray
+
+//problem 1
 let k = 5;
 let arr: number[] = [1, 3, 2, 6, -1, 4, 1, 8, 2];
+
+// problem 2
+let k3 = 3;
+let arr2 = [2, 1, 5, 1, 3, 2];
 
 let SlidingWindow: ISlidingWindow = {
   start: 0,
@@ -30,11 +42,10 @@ let SlidingWindow: ISlidingWindow = {
   locakWindowAtIndex: 4,
   sum: 0,
   result: [],
-  updateWindow: (window, capturedItem, capturedItemIndex) => {
+  updateWindow: (window, capturedItem, capturedItemIndex, targetArr) => {
     //if the window is not locked and the loop is currenly running
     window.sum += capturedItem;
 
-    // if the window is about to be locked
     // when to lock the window
     if (capturedItemIndex >= window.locakWindowAtIndex) {
       window.locked = true;
@@ -43,14 +54,53 @@ let SlidingWindow: ISlidingWindow = {
     if (window.locked) {
       let avrg = window.sum / 5;
       window.result.push(avrg);
-      window.sum -= arr[window.start];
+      window.sum -= targetArr[window.start];
       window.start++;
     }
   },
 };
 
 for (let i = 0; i < arr.length; i++) {
-  SlidingWindow.updateWindow(SlidingWindow, arr[i], i);
+  SlidingWindow.updateWindow(SlidingWindow, arr[i], i, arr);
 }
 
 console.log(SlidingWindow.result);
+
+let maxSumSubArraySlidingWindow: ISlidingWindow = {
+  locakWindowAtIndex: 2,
+  locked: false,
+  result: [],
+  start: 0,
+  sum: 0,
+  store: 0, // we will store here the maximum subarray
+  updateWindow: (window, capturedItem, capturedIndex, targetArr) => {
+    window.sum += capturedItem;
+
+    //when to lock the window ?
+    if (capturedIndex >= window.locakWindowAtIndex) {
+      window.locked = true;
+    }
+
+    //calculate the sum
+    if (window.locked) {
+      // what to do ?
+      window.result.push(window.sum);
+      if (window.sum > window.store) {
+        window.store = window.sum;
+      }
+      window.sum -= targetArr[window.start];
+      window.start++;
+    }
+  },
+};
+
+for (let i = 0; i < arr2.length; i++) {
+  maxSumSubArraySlidingWindow.updateWindow(
+    maxSumSubArraySlidingWindow,
+    arr2[i],
+    i,
+    arr2
+  );
+}
+
+console.log(maxSumSubArraySlidingWindow.store);
